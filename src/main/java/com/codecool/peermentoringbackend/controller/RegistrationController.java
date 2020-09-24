77bbae9d989f57f267fca5980f4e14a64bc9de05
sibaluca12 <sibaluca@gmail.com>
2020-09-24 12:28:41 +0200

    cors

--- src/main/java/com/codecool/peermentoringbackend/controller/RegistrationController.java
+++ src/main/java/com/codecool/peermentoringbackend/controller/RegistrationController.java
@@ -3,13 +3,11 @@ package com.codecool.peermentoringbackend.controller;
 import com.codecool.peermentoringbackend.model.UserModel;
 import com.codecool.peermentoringbackend.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reg")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegistrationController {
 
     @Autowired
--- src/main/java/com/codecool/peermentoringbackend/controller/SearchController.java
+++ src/main/java/com/codecool/peermentoringbackend/controller/SearchController.java
@@ -1,13 +1,21 @@
 package com.codecool.peermentoringbackend.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class SearchController {
+
+    @RequestMapping(value = "/search/{urlParam}", method = GET)
+    @ResponseBody
+    public String search( @PathVariable String urlParam) {
+        String[] splitted = urlParam.split("\\s+");
+        String joinedString = String.join(" AND ", splitted);
+        return "helo";
+    }
 
 
 
